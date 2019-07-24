1b9520355df09ae6c08a0c785528083e0ba6cbdc
Nhat Nguyen <phongnhat19@gmail.com>
2019-07-24 15:49:04 +0700

    update siteConfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -25,15 +25,15 @@ const users = [
 const siteConfig = {
   title: 'kintone UI Component', // Title for your website.
   tagline: 'Documentation for kintone UI Component',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://phongnhat19.github.io', // Your website URL
+  baseUrl: '/kuc-doc/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
   projectName: 'kuc-doc',
-  organizationName: 'Cybozu',
+  organizationName: 'phongnhat19',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
